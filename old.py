31002ecfc278a8b05fc88ab2cbcfa10254f06320
James Banks <jbanks@hey.com>
2022-04-02 16:12:04 +1100

    Remove Print and check

--- old.py
+++ old.py
@@ -1,12 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
 GPIO.setup(12, GPIO.OUT)
-print("LED on")
 GPIO.output(12, GPIO.HIGH)
 time.sleep(1)
-print("LED off")
 GPIO.output(12, GPIO.LOW)
 time.sleep(1)
-print("Yes")
