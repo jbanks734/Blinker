7f46a7fa22588c1d230b6fd3b23ae7803f664758
James <jbanks@hey.com>
2022-04-02 16:05:16 +1100

    Remove Print

--- blinker.py
+++ blinker.py
@@ -5,10 +5,8 @@ GPIO.setup(12, GPIO.OUT)
 
 try:
     while 1:
-        print("LED on")
         GPIO.output(12, GPIO.HIGH)
         time.sleep(.25)
-        print("LED off")
         GPIO.output(12, GPIO.LOW)
         time.sleep(.25)
 except KeyboardInterrupt:
