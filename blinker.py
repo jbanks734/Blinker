65cd422b4719830e5ecb480d3b657ab485f8b9aa
James <jbanks@hey.com>
2022-04-02 15:56:18 +1100

    While Loop

--- blinker.py
+++ blinker.py
@@ -2,9 +2,14 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
-print("LED on")
-GPIO.output(12, GPIO.HIGH)
-time.sleep(1)
-print("LED off")
-GPIO.output(12, GPIO.LOW)
-GPIO.cleanup()
+
+try:
+    while 1:
+        print("LED on")
+        GPIO.output(12, GPIO.HIGH)
+        time.sleep(.25)
+        print("LED off")
+        GPIO.output(12, GPIO.LOW)
+        time.sleep(.25)
+except KeyboardInterrupt:
+    GPIO.cleanup()
